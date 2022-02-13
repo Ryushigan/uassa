f7810b47d9fc2cfa10a4ad18043f55d1fa6eda42
Ryushigan <82567822+Ryushigan@users.noreply.github.com>
2022-02-13 18:23:25 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,15 @@
+<?php
 
+	$conn = mysqli_connect("172.19.0.2","root","root123","trucorp");
+
+	$query = "SELECT * FROM users";
+
+	$exec = $conn->query($query);
+  
+  echo $exec->num_rows;
+
+	while($data = $exec->fetch_assoc()) {
+		print_r($data);
+	}
+
+?>
